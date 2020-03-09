12620ef63494328d1c03e5b6fa9907ecd70e4689
jiangwneyang <u19950930@gmail.com>
2020-03-10 00:24:23 +0800

    feat: 添加purgecss支持

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+
+  css: ["./styles/**/*.css"],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-preset-env")]
+  plugins: [
+    require("tailwindcss"),
+    require("postcss-preset-env"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };
